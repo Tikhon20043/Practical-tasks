42ef4cbbc28916f4f7af7fa6f8ce86e9e9bf71ea
Tikhon20043 <tihonfedoseev65@gmail.ru>
2024-03-23 14:10:35 +0300

    .add

---
+++ PZ_13/PZ_13_1.py
@@ -0,0 +1,17 @@
+#1. Сгенерировать матрицу, в которой элементы больше 10 заменяются на 0.
+
+# Генерация исходной матрицы
+original_matrix = [[i*j for j in range(1, 5)] for i in range(1, 5)]
+
+# Вывод исходной матрицы
+print("Исходная матрица:")
+for row in original_matrix:
+    print(row)
+
+# Изменение матрицы: замена элементов > 10 на 0
+modified_matrix = [[0 if element > 10 else element for element in row] for row in original_matrix]
+
+# Вывод изменённой матрицы
+print("\nИзменённая матрица:")
+for row in modified_matrix:
+    print(row)
---
+++ PZ_13/PZ_13_2.py
@@ -0,0 +1,16 @@
+#В квадратной матрице все элементы, не лежащие на главной диагонали увеличить в 2 раза.
+
+# Создание и вывод исходной матрицы
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
+print("Исходная матрица:")
+for row in matrix:
+    print(row)
+
+# Преобразование  матрицы
+transformed_matrix = [[row[i] if i == index else row[i] * 2 for i in range(len(row))] for index, row in enumerate(matrix)]
+
+print("Преобразованная мартица:")
+for row in transformed_matrix:
+    print(row)
